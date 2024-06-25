45db00553d168e3933b5727dcdd67e6b89924d2e
Tim Mo <timxymo@gmail.com>
2024-06-25 19:36:42 -0400

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+let slideIndex = 1;
+let slideText = ["Sample text 1", "Sample text 2", "Sample text 3", "Sample text 4"];
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    document.getElementById("slideText").textContent = slideText[slideIndex-1];
+}
