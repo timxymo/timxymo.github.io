b94b4b5b3d47abbed662ea7e5b6d3e462fd9511d
Tim Mo <xmo1225@arizona.edu>
2024-10-02 23:04:42 -0700

    update furniture-entrywayTable page

--- js/furniture-entrywayTable.js
+++ js/furniture-entrywayTable.js
@@ -13,7 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("slides");
+    let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
